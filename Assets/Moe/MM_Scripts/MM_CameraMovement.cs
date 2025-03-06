0420303d0d85d829c1f185aaeba0ac5dec928c27
Moe-beep <moe.naing@vuoksi.fi>
2025-03-06 14:20:35 +0200

    camera script

---
+++ Assets/Moe/MM_Scripts/MM_CameraMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MM_CameraMovement : MonoBehaviour
+{
+    public float mouseSensitivity = 100.0f;
+
+    private Transform playerBody;
+
+    private float xRotation = 0.0f;
+
+    private void Start()
+
+    {
+
+        playerBody = transform.parent;
+
+        Cursor.lockState = CursorLockMode.Locked; // lock the cursor in the center of the screen
+
+    }
+
+    private void Update()
+
+    {
+
+        RotateCamera();
+
+    }
+
+    private void RotateCamera()
+
+    {
+
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation, -90.0f, 90.0f); // so the camera doesn't rotate upside down
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+
+    }
+}
