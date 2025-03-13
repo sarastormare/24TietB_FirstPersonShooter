9d8a2b0094c355a7ab1fbe7aa1386bf8c7cf8a7b
sarastormare <saraseddighi83@gmail.com>
2025-03-13 13:27:03 +0200

    Player Movement

---
+++ Assets/Sara/SS_Scripts/SS_PlayerMovement.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+
+public class SS_PlayerMovement : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public float jumpHeight = 2.0f;
+    public float groundDistance = 0.4f;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    private CharacterController characterController;
+    private Transform cameraTransform;
+    private Vector3 velocity;
+    private bool isGrounded;
+    private float gravity = -9.81f;
+
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        cameraTransform = Camera.main.transform;
+
+        if (groundCheck == null)
+        {
+            Debug.LogError("Assign a GroundCheck object to the PlayerMovement script in the inspector.");
+            this.enabled = false;
+            return;
+        }
+    }
+
+    private void Update()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundLayer);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f; // keep the character on the ground
+        }
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 forwardDirection = cameraTransform.forward;
+        Vector3 rightDirection = cameraTransform.right;
+
+        forwardDirection.y = 0; // so that the player doesn't fly on the y axis.
+        rightDirection.y = 0;
+
+        forwardDirection.Normalize();
+        rightDirection.Normalize();
+
+        Vector3 desiredDirection = (forwardDirection * vertical + rightDirection * horizontal).normalized;
+        Vector3 movement = desiredDirection * speed * Time.deltaTime;
+        characterController.Move(movement);
+        // jump logic
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        // velocity.y += Gravity * Time.deltaTime;
+        characterController.Move(velocity * Time.deltaTime);
+    }
+}
